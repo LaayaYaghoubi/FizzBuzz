0456c7d46261d28778d8622cae4d1590d8324b0a
Laaya <layayaghoubi@gmail.com>
2022-04-24 11:01:57 +0430

    fizzbuzz project files added

---
+++ src/FizzBuzz.Logic.Tests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Logic.Tests
+{
+    public class FizzBuzzTests
+    {
+       
+        [Fact]
+        public void irritated_number_is_divisible_by_3()
+        {
+
+
+        }
+    }
+}
